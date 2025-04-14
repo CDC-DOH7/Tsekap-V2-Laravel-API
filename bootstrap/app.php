4beac032fa1d28cbf1c2616f17aa899fe333142a
CDC RESU DOH-7, Elijah Nicholas Esguerra <doh7cdc.dev@gmail.com>
2025-04-14 10:56:43 +0800

    added changes to the bootstrap method

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,9 +6,9 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -24,4 +24,10 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })->create()
+    ->app()->router->group([
+        'prefix' => 'api',
+        'middleware' => ['api'],
+    ], function () {
+        require base_path('routes/api.php');
+    });
