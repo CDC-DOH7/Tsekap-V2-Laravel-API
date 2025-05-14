e031ceb2a96b86eb50350d86ad64e988f503e5a7
CDC RESU DOH-7, Elijah Nicholas Esguerra <doh7cdc.dev@gmail.com>
2025-05-14 12:58:19 +0800

    added fix for those without no email as fields

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,7 +32,7 @@ class AuthController extends Controller
             'fields.password' => 'required|string|min:8|max:255',
             'fields.contact' => 'required|string|max:11',
             'fields.user_priv' => 'required|integer',
-            'fields.email' => 'nullable|string|max:255|email',
+            'fields.email' => 'sometimes|nullable|string|max:255|email',
         ]);
 
         // Trigger validation and return 422 if it fails
@@ -67,7 +67,7 @@ class AuthController extends Controller
                 'contact' => $validatedFields['contact'],
                 'user_priv' => $validatedFields['user_priv'],
                 'verified' => 0, // make this field zero because it is self-registered and still needs to be verified
-                'email' => $validatedFields['email'] ?? null,
+                'email' => !empty($validatedFields['email']) ? $validatedFields['email'] : null,
             ]);
 
             $userHfMapping = UserHealthFacility::create([
