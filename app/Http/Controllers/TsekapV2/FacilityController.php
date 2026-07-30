febfff7fd6cc4a7b480e856bd44a740441d8d9ae
DOH-7 CDC Elijah Nicholas Esguerra <doh7cdc.dev@gmail.com>
2026-07-30 17:14:55 +0800

    removed unused references

--- app/Http/Controllers/TsekapV2/FacilityController.php
+++ app/Http/Controllers/TsekapV2/FacilityController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\TsekapV2;
 
 use Illuminate\Http\Request;
-use App\Models\User;
 use App\Models\TsekapV2\Facilities;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
